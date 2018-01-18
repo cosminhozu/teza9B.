cef485587271afa4220a1086188beab6b3adb2ba
cosminhozu <35564379+cosminhozu@users.noreply.github.com>
2018-01-18 13:58:41 +0200

    Add files via upload

---
+++ HozuCosminteza9B.cpp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{ int  nr ,nr_copie ,cifra  ;
+nr=nr_copie;
+cout<<"da o valoare numarului: ";cin>>nr;
+if(nr==0)
+{ cou<<" cel mai mic numar format din cifrele numarului este ";<<0<<endl;}
+ while (nr>0)
+   { cifra=nr%10;
+   nr=nr/10;
+
+
+
+   }
+
+
+
+
+
+}
